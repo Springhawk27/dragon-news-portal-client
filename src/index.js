bd1abad358e34307e116c7312f1a05b7f589572e
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-23 22:33:04 +0600

    auth context added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 // bootstrap cdn
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
